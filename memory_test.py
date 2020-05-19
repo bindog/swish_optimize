ac8ef825785dbb1f34d6a1ccc6828ede790728ba
bindog <bindog@outlook.com>
2020-05-19 20:32:24 +0800

    fix bugs and add inference time calc

--- memory_test.py
+++ memory_test.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import torch
 import torch.nn as nn
@@ -40,20 +41,28 @@ def convert_recursive(module, swish_act):
 
 
 def train_loop(model, optimizer, criterion):
-    random_data = torch.rand((8, 3, 224, 224)).cuda()
-    random_label = torch.rand((1000,)).cuda()
-    for _ in range(1e6):
+    random_data = torch.rand((32, 3, 224, 224)).cuda()
+    random_label = torch.randint(1000, (32, ), dtype=torch.int64).cuda()
+    print("start training now...please check GPU memory with nvidia-smi")
+    print("Stop the program by Ctrl-C")
+    for step in range(1000000):
+        torch.cuda.synchronize()
+        start = time.time()
         optimizer.zero_grad()
         output = model(random_data)
         loss = criterion(output, random_label)
         loss.backward()
         optimizer.step()
+        torch.cuda.synchronize()
+        end = time.time()
+        if step % 20 == 0 and step > 0:
+            print("forward and backward time: ", end - start)
 
 
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     model = resnet50()
-    model = replace_relu_with_swish(model, "v1").cuda()
+    model = replace_relu_with_swish(model, "v2").cuda()
     optimizer = optim.SGD(model.parameters(), lr=0.01)
     criterion = nn.CrossEntropyLoss().cuda()
 
